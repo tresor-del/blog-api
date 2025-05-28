33dd4ce3abd8f0811e0545982d80e4569d8e85f2
tresor-del <tresoradzanto7@gmail.com>
2025-05-28 05:32:39 +0000

    Add is_active field to the Article model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class Article(BaseModel):
     id: int
     title: str
     content: str
+    is_active: bool = Field(default=True)
