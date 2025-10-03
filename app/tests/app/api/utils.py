64d38478ea5d3bedf86c5509e2046181d51388cf
tresor-del <tresoradzanto7@gmail.com>
2025-10-03 15:48:23 +0000

    Modification du workflow

--- app/tests/app/api/utils.py
+++ app/tests/app/api/utils.py
@@ -7,7 +7,44 @@ def create_article_f(db: Session):
     
     article = ArticleCreate(
         title="Random article",
-        content= "Random content"
+        content= """
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed ullam necessitatibus velit ducimus quasi laborum repellendus assumenda delectus nam facilis, laudantium voluptate quisquam, sapiente voluptatem ipsum impedit porro provident.
+        """
     )
     
     return create_article(db=db, data=article)
